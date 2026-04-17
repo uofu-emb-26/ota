ef2016a253d25fb12f8960421d0d07f079189204
reporterfredbriggs <nickbaret5@gmail.com>
2026-04-17 17:54:28 -0600

    added starting arduino UART code in esp32 dir

--- OTA/ota_esp32/src/main.cpp
+++ OTA/ota_esp32/src/main.cpp
@@ -1,31 +1,39 @@
 #include <Arduino.h>
-
+#include <iterator>
+#include <Serial.h>
 #define LED_DELAY 500U
 // put function declarations here:
-int myFunction(int, int);
-void togglePin(int pin);
+uint32_t write(const char *buffer, uint8_t size);
+void setup();
+void loop();
+void flush(bool txOnly);
+bool setClockSource(SerialClkSrc clkSrc);
+bool setMode(SerialMode mode);
 
+// using GPIO1 (TX0) and GPIO3 (RX0) for ESP32 -> STM connection
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.begin(115200);
-  delay(500); // Wait for serial to initialize
-  Serial.println("Hello, World!");
-  pinMode(LED_BUILTIN, OUTPUT);
+  //set the UART clock source
+  Serial1.setClockSource(UART_CLK_SRC_DEFAULT);
+
+  //set the UART operating mode
+  Serial1.setMode(UART_MODE_UART);
+  
+  // Initializes the Serial port with the specified baud rate and default configuration.
+  Serial1.begin(115200)
+
+  const char *buffer = "Hello world";
+  uint8_t size = sizeof(buffer);
+  uint32_t bytes_written = Serial1.write(buffer, size);
+
+  /* Waits for all data in the TX buffer to be transmitted. */
+  flush(true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  togglePin(LED_BUILTIN);
-  delay(LED_DELAY);
+  
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
-}
-
-//toggle pin
-void togglePin(int pin) {
-  digitalWrite(pin, !digitalRead(pin));
 }
