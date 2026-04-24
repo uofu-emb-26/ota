8efe471e1ffd5fc8841880547f218181b18a286e
Jeffrey Hansen <jhansenskier@gmail.com>
2026-04-23 23:18:24 -0600

    added flash update communication protocol

--- OTA/Core/Src/common/flash_update.c
+++ OTA/Core/Src/common/flash_update.c
@@ -108,6 +108,104 @@ int flash_write_buf(uint32_t dest_addr, const uint8_t *buf, uint32_t len)
     return 0;
 }
 
+int flash_clr_upd_region(uint32_t start_addr, uint32_t page_total){
+    flash_unlock();
+    uint32_t i=0U;
+    uint32_t current_addr = start_addr;
+    while(i < page_total){
+        if(flash_erase_page(start_addr) != 0){
+            flash_lock();
+            return 1;
+        }
+        current_addr += 2048;
+        i++;
+    }
+
+    flash_lock();
+    return 0;
+}
+
+
+
+int flash_write_from_uart(USART_TypeDef *uart, uint32_t page_total){ //assumes fully initialized uart peripheral
+
+    uint8_t current_slot = get_current_slot();
+    uint32_t start_address;
+
+    uint8_t type_byte;
+    uint8_t first_byte;
+    uint8_t second_byte;
+    uint16_t write_data;
+
+    uint32_t memory_end;
+
+    if(current_slot == 1){
+        start_address = 0x08011800;
+    } else if(current_slot == 2){
+        start_address = 0x08004000;
+    } else {
+        transmit_char(0xFF, uart);
+        return -1;
+    }
+
+    memory_end = start_address + ((page_total) * 2048);
+
+    if (flash_clr_upd_region(start_address) != 0) {
+      return -1;
+    }
+    
+    do {
+
+      transmit_char(0, uart); //TODO THIS FUNCTION EXPLICITLY EXCEPTS BEHAVIOR COULD BE ADDED
+                              //DICTATING WAIT OR DENY
+
+      type_byte = recieve_char(uart);
+      first_byte = recieve_char(uart);
+      second_byte = recieve_char(uart);
+
+      write_data = (((uint16_t)first_byte << 8) | ((uint16_t)second_byte));
+
+      if (type_byte == 0) {
+        break;
+      } else if (type_byte == 1) {
+
+        if ((start_address + 1) >= memory_end) {
+          transmit_char(0xFF, uart);
+          return -1;
+        }
+
+        if (flash_write(start_address, write_data) != 0) {
+          transmit_char(0xFF, uart);
+          return -1;
+        }
+
+        start_address += 2;
+        transmit_char(1, uart);
+
+      } else if (type_byte == 2) {
+        // placeholder for crc_value handling or other data_type handling
+        transmit_char(2, uart);
+      } else {
+        transmit_char(0xFF, uart);
+        return -1;
+      }
+
+    } while (1);
+
+    return 0;
+}
+
+void transmit_char(uint8_t out, USART_TypeDef *uart){
+    while(!(uart->ISR & USART_ISR_TXE)){}
+    uart->TDR = out;
+}
+
+char receive_char(USART_TypeDef uart){
+    while(!(uart->ISR & USART_ISR_RXNE)){}
+    return (char)uart->RDR;
+}
+
+
 /* ---------------------------------------------------------------------------
  * button_interrupt_config  (unchanged – EXTI0 on PA0)
  * ---------------------------------------------------------------------------*/
