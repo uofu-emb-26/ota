28944c822dedb88ca117ed80be03974004d58431
reporterfredbriggs <nickbaret5@gmail.com>
2026-04-25 15:30:07 -0600

    removed sensitive info

--- OTA/ota_esp32/src/main.cpp
+++ OTA/ota_esp32/src/main.cpp
@@ -10,10 +10,10 @@
 
 #define LED_DELAY 250U
 #define BINARY_MAX_SIZE 14000 // current size is 12708, could change later
-#define SSID "LeChalet2.4Gz"
-#define PASS "1001ABNBEmpire!"
-#define LOCAL_IP "http://10.0.0.88:8080/OTA_app_a.bin"
-#define VERSION_TXT "http://10.0.0.88:8080/version.txt"
+#define SSID "xx"
+#define PASS "xx"
+#define LOCAL_IP "http://x.x.x.x:8080/OTA_app_a.bin"
+#define VERSION_TXT "http://x.x.x.x:8080/version.txt"
 
 // put function declarations here:
 int myFunction(int, int);
@@ -243,7 +243,8 @@ void readBinaryFromLittleFS(uint8_t* buffer, int* size) {
   }
   *size = file.read(buffer, BINARY_MAX_SIZE);
   file.close();
-  Serial.println("Binary read from LittleFS");
+  Serial.println("Binary read from LittleFS, read this many bytes: ");
+  Serial.print(*size);
 }
 
 // HTTP handler: reads binary from LittleFS and returns size + first 16 byte
