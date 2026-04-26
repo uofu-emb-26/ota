f80417d10a87b21db0d38a4042d71d1b1c666f94
Jeffrey Hansen <jhansenskier@gmail.com>
2026-04-25 22:18:28 -0600

    fixed adding example instead of only actual file that will disapear

--- OTA/ota_esp32/include/sensitive_data.h
+++ OTA/ota_esp32/include/sensitive_data.h
@@ -1,6 +1,6 @@
 //Add this file to git ignore then update this file with your actual passwords and ip info
-#define SSID "YOUR_WIFI_NAME"
-#define PASS "YOUR_WIFI_PASSWORD"
-#define Local_IP "192.168.x.x"
-#define UPDATE_VERSION_URL "http://192.168.x.x:8000/version.txt"
-#define UPDATE_BINARY_URL "http://192.168.x.x:8000/firmware.bin"
+#define SSID "Jeffrey's S24 Ultra"
+#define PASS "gmuo234!"
+#define Local_IP "10.234.41.56"
+#define UPDATE_VERSION_URL "http://10.234.41.56:8000/version.txt"
+#define UPDATE_BINARY_URL "http://10.234.41.56:8000/OTA_app_a.bin"
---
+++ OTA/ota_esp32/include/sensitive_data_example.h
@@ -0,0 +1,6 @@
+//Create sensitive_data.h and copy this template to it then add your actual info
+#define SSID "YOUR WIFI SSID"
+#define PASS "YOUR PASSWORD"
+#define Local_IP "YOUR ACTUAL IP ADDRESS"
+#define UPDATE_VERSION_URL "http://YOURIPHERE:8000/version.txt"
+#define UPDATE_BINARY_URL "http://YOURIPHERE:8000/THEFILETOTRANSFER.bin"
